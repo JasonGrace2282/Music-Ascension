013b10c9f6ef83da185c2408f414647803516b1e
EchoCodez <mihir.nimkar@gmail.com>
2022-08-13 10:52:06 -0400

    bug fixes for jumping and timing

--- code/player.py
+++ code/player.py
@@ -25,6 +25,8 @@ class TeleportPlayer(pygame.sprite.Sprite):
         self.end = 0
         self.counter = 0
         self.delta = 0
+        self.correctnote = False
+        self.level_note = "G"
     
     def set_image(self):
         
@@ -55,6 +57,21 @@ class TeleportPlayer(pygame.sprite.Sprite):
         self.delta = delta
         self.direction.x = 0
 
+        if keys[pygame.K_c]:
+            self.level_note = "C"
+        elif keys[pygame.K_d]:
+            self.level_note = "D"
+        elif keys[pygame.K_e]:
+            self.level_note = "E"
+        elif keys[pygame.K_f]:
+            self.level_note = "F"
+        elif keys[pygame.K_g]:
+            self.level_note = "G"
+        elif keys[pygame.K_a]:
+            self.level_note = "A"
+        elif keys[pygame.K_b]:
+            self.level_note = "B"
+
         self.teleport()
 
     def update_status(self):
@@ -78,15 +95,15 @@ class TeleportPlayer(pygame.sprite.Sprite):
             pass
         elif 0.1 <= self.delta < 0.4:
             print("mission failed, we'll get e'm next time")
-        elif 0.4 <= self.delta < 0.6:
+        elif 0.4 <= self.delta < 0.6 and self.correctnote:
             print("0.5")
             self.direction.y -= 32
             self.rect.y += self.direction.y
             self.direction.y += 32
-            self.direction.x += 24
+            self.direction.x += 16
         elif 0.6 <= self.delta < 0.9:
             print("mission failed, we'll get e'm next time")
-        elif 0.9 <= self.delta < 1.1:
+        elif 0.9 <= self.delta < 1.1 and self.correctnote:
             print("1")
             self.direction.y -= 64
             self.rect.y += self.direction.y
@@ -94,36 +111,36 @@ class TeleportPlayer(pygame.sprite.Sprite):
             self.direction.x += 24
         elif 1.1 <= self.delta < 1.4:
             print("mission failed, we'll get e'm next time")
-        elif 1.4 <= self.delta < 1.6:
+        elif 1.4 <= self.delta < 1.6 and self.correctnote:
             print("1.5")
-            self.direction.y -= 64
+            self.direction.y -= 96
             self.rect.y += self.direction.y
-            self.direction.y += 64
+            self.direction.y += 96
             self.direction.x += 32
         elif 1.6 <= self.delta < 1.9:
             print("mission failed, we'll get e'm next time")
-        elif 1.9 <= self.delta < 2.1:
+        elif 1.9 <= self.delta < 2.1 and self.correctnote:
             print("2")
             self.direction.y -= 128
             self.rect.y += self.direction.y
             self.direction.y += 128
-            self.direction.x += 24
+            self.direction.x += 40
         elif 2.1 <= self.delta < 2.9:
             print("mission failed, we'll get e'm next time")
-        elif 2.9 <= self.delta < 3.1:
+        elif 2.9 <= self.delta < 3.1 and self.correctnote:
             print("3")
-            self.direction.y -= 128
+            self.direction.y -= 192
             self.rect.y += self.direction.y
-            self.direction.y += 128
-            self.direction.x += 32
+            self.direction.y += 192
+            self.direction.x += 48
         elif 3.1 <= self.delta < 3.9:
             print("mission failed, we'll get e'm next time")
-        elif 3.9 <= self.delta < 4.1:
+        elif 3.9 <= self.delta < 4.1 and self.correctnote:
             print("4")
             self.direction.y -= 256
             self.rect.y += self.direction.y
             self.direction.y += 256
-            self.direction.x += 32
+            self.direction.x += 56
         elif self.delta > 4.1:
             print("mission failed, we'll get e'm next time")
     
