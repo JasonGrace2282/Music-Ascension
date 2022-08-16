a3b5cfb179e987cd31319a90c24b42675d911701
EchoCodez <mihir.nimkar@gmail.com>
2022-08-16 14:32:15 -0400

    fixed files in main

--- code/main.py
+++ code/main.py
@@ -15,13 +15,13 @@ class Game:
         # Background
         self.background1 = pygame.image.load("resources/frontpage.jpg")
         self.levelBackground = pygame.image.load("resources/background.jpg")
-        self.intermediateImage = pygame.image.load("resources/FinalIntermediateImage.png")
+        self.intermediateImage = pygame.image.load("resources/intermediate.png")
         self.startButtonImage = pygame.image.load("resources/start.png")
         self.creditsButtonImage = pygame.image.load("resources/credits.png")
         self.nextButtonImage = pygame.image.load("resources/next.png")
-        self.beginnerMap = pygame.image.load("resources/FinalBeginnerMap.png")
-        self.beginnerTopicsCovered = pygame.image.load("resources/FinalBeginnerTopics.png")
-        self.beginnerImage = pygame.image.load("resources/FinalBeginnerImage.png")
+        self.beginnerMap = pygame.image.load("resources/beginnermap.jpg")
+        self.beginnerTopicsCovered = pygame.image.load("resources/beginnertopics.jpg")
+        self.beginnerImage = pygame.image.load("resources/beginner.png")
 
         # Variables
         self.startGame = False
