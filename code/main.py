a59998e538c1c941ef9559dc508423571641d21a
EchoCodez <mihir.nimkar@gmail.com>
2022-08-18 14:11:24 -0400

    fixed formatting issues

--- code/main.py
+++ code/main.py
@@ -1,8 +1,4 @@
-from hashlib import blake2b
-from re import T
-import pygame
-import sys
-import time
+import pygame, sys, time
 from setup import *
 from level import TeleportLevel, NoteLevel
 
@@ -21,14 +17,12 @@ class Game:
         # Background
         self.background1 = pygame.image.load("resources/frontpage.jpg")
         self.levelBackground = pygame.image.load("resources/background.png")
-        self.intermediateImage = pygame.image.load(
-            "resources/intermediate.png")
+        self.intermediateImage = pygame.image.load("resources/intermediate.png")
         self.startButtonImage = pygame.image.load("resources/start.png")
         self.creditsButtonImage = pygame.image.load("resources/credits.png")
         self.nextButtonImage = pygame.image.load("resources/next.png")
         self.beginnerMap = pygame.image.load("resources/beginnermap.jpg")
-        self.beginnerTopicsCovered = pygame.image.load(
-            "resources/beginnertopics.jpg")
+        self.beginnerTopicsCovered = pygame.image.load("resources/beginnertopics.jpg")
         self.beginnerImage = pygame.image.load("resources/beginner.png")
         self.informationPage1 = pygame.image.load("resources/NDNotes.png")
         self.NDhow2play = pygame.image.load("resources/NDdirections.png")
@@ -59,20 +53,14 @@ class Game:
         self.sleepCounter2 = 0
         self.nextCounter = 0
         self.counter = 0
-        self.beginnerRect = pygame.Rect(600-self.beginnerImage.get_width(
-        )/2, 50, self.beginnerImage.get_width(), self.beginnerImage.get_height())
-        self.startRect = pygame.Rect(
-            540, 200, self.startButtonImage.get_width(), self.startButtonImage.get_height())
-        self.intermediateRect = pygame.Rect(600-self.intermediateImage.get_width(
-        )/2, 400, self.intermediateImage.get_width(), self.intermediateImage.get_height())
-        self.creditsButton = pygame.Rect(
-            530, 300, self.creditsButtonImage.get_width(), self.creditsButtonImage.get_height())
-        self.nextButton = pygame.Rect(
-            898, 582, self.nextButtonImage.get_width(), self.nextButtonImage.get_height())
+        self.beginnerRect = pygame.Rect(600-self.beginnerImage.get_width()/2, 50, self.beginnerImage.get_width(), self.beginnerImage.get_height())
+        self.startRect = pygame.Rect(540, 200, self.startButtonImage.get_width(), self.startButtonImage.get_height())
+        self.intermediateRect = pygame.Rect(600-self.intermediateImage.get_width()/2, 400, self.intermediateImage.get_width(), self.intermediateImage.get_height())
+        self.creditsButton = pygame.Rect(530, 300, self.creditsButtonImage.get_width(), self.creditsButtonImage.get_height())
+        self.nextButton = pygame.Rect(898, 582, self.nextButtonImage.get_width(), self.nextButtonImage.get_height())
         self.noteDurationStage1 = pygame.Rect(28, 341, 139, 188)
         self.noteDurationStage2 = pygame.Rect(225, 340, 135, 185)
-        self.noteDurationStartRect = pygame.Rect(
-            898, 582, self.nextButtonImage.get_width(), self.nextButtonImage.get_height())
+        self.noteDurationStartRect = pygame.Rect(898, 582, self.nextButtonImage.get_width(), self.nextButtonImage.get_height())
         self.NDstage2 = pygame.Rect(400, 340, 140, 215)
         self.pizza_man_2 = pygame.Rect(605, 340, 140, 215)
         self.NDstage3 = pygame.Rect(830, 340, 140, 215)
@@ -106,7 +94,7 @@ class Game:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.startRect.collidepoint(event.pos):
                     self.startGame = True
-                    print('START Button Clicked')
+                    print("START Button Clicked")
                     time.sleep(0.5)
                     self.levelConfirm = True
                     self.offCreditButton = True
@@ -116,18 +104,15 @@ class Game:
             if self.startGame:
                 self.screen.fill(0)
                 self.screen.blit(self.levelBackground, (0, 0))
-                self.screen.blit(self.beginnerImage,
-                                 (600-self.beginnerImage.get_width()/2, 50))
-                self.screen.blit(self.intermediateImage, (600-self.intermediateImage.get_width(
-                )/2, 400, self.intermediateImage.get_width(), self.intermediateImage.get_height()))
+                self.screen.blit(self.beginnerImage,(600-self.beginnerImage.get_width()/2, 50))
+                self.screen.blit(self.intermediateImage, (600-self.intermediateImage.get_width())/2, 400, self.intermediateImage.get_width(), self.intermediateImage.get_height())
 
             if not self.offCreditButton:
                 if self.creditsClicked:
                     self.screen.fill((255, 255, 255))
                     creditsText = pygame.font.SysFont(None, 30)
-                    creditsText = creditsText.render('Credits:', True, 0)
-                    creditsText2 = (pygame.font.SysFont(None, 30)).render(
-                        'Insert Image Credits Here', True, 100)
+                    creditsText = creditsText.render("Credits:", True, 0)
+                    creditsText2 = (pygame.font.SysFont(None, 30)).render("Insert Image Credits Here", True, 100)
                     self.screen.blit(creditsText, (0, 0))
                     self.screen.blit(creditsText2, (0, 100))
                     self.screen.fill(0, rect=(0, 480, 200, 123))
@@ -143,16 +128,13 @@ class Game:
                 if self.interClicked:
                     self.screen.fill((255, 255, 255))
                     interTopicsText = pygame.font.SysFont(None, 40)
-                    interTopicsText = interTopicsText.render(
-                        'Topics Covered: ', True, 0)
+                    interTopicsText = interTopicsText.render("Topics Covered: ", True, 0)
                     self.screen.blit(interTopicsText, (0, 0))
                     interTopicsText2 = pygame.font.SysFont(None, 40)
-                    interTopicsText2 = interTopicsText2.render(
-                        'Dynamics and Articulation', True, 0)
+                    interTopicsText2 = interTopicsText2.render("Dynamics and Articulation", True, 0)
                     self.screen.blit(interTopicsText2, (0, 50))
                     interTopicsText3 = pygame.font.SysFont(None, 40)
-                    interTopicsText3 = interTopicsText3.render(
-                        'Time Signatures', True, 0)
+                    interTopicsText3 = interTopicsText3.render("Time Signatures", True, 0)
                     self.screen.blit(interTopicsText3, (0, 100))
                     self.screen.blit(self.nextButtonImage, (898, 592))
 
@@ -240,11 +222,9 @@ class Game:
 
                 if self.stageChooser2:
                     self.screen.fill((255, 255, 255))
-                    title = (pygame.font.SysFont(None, 40)
-                             ).render('Notes', True, 0)
+                    title = (pygame.font.SysFont(None, 40)).render("Notes", True, 0)
                     self.screen.blit(title, (0, 0))
-                    self.screen.blit(self.nextButtonImage, (898, 582, self.nextButtonImage.get_width(
-                    ), self.nextButtonImage.get_height()))
+                    self.screen.blit(self.nextButtonImage, (898, 582, self.nextButtonImage.get_width(), self.nextButtonImage.get_height()))
 
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         if self.noteDurationStartRect.collidepoint(event.pos):
