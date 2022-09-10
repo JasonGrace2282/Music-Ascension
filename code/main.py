92a8e29ecdce54b6078f002b5ca1764ef57f7af0
JasonGrace2282 <aarush.deshpande@gmail.com>
2022-09-10 14:08:11 -0400

    Changed image file

--- code/main.py
+++ code/main.py
@@ -34,7 +34,7 @@ class Game:
         self.button = pygame.image.load("../resources/copybutton3.png")
         self.pizzaNotes1 = pygame.image.load("../resources/treble_notes.png")
         self.pizzaPlay = pygame.image.load("../resources/pizza_notes.png")
-        self.pizzaBackground = pygame.image.load("../resources/backgroundpizza.png")
+        self.pizzaBackground = pygame.image.load("../resources/backgroundpizza2.png")
         self.creditsImage = pygame.image.load("../resources/creditsbackground.png")
         self.copiedtxt = pygame.image.load("../resources/copied.png")
         self.starbackground = pygame.image.load("../resources/starbg.png")
