4e588fc1f77340fede792886973b35e200464e25
JasonGrace2282 <aarush.deshpande@gmail.com>
2022-10-05 08:17:00 -0400

    ran a test

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
+from time import perf_counter
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((1200, 790))
 counter = 0
 my_list = [0]
-for i in my_list:
-    print(i)
-    my_list.append(i)
+run = True
+start = perf_counter()
+while run:
+    print(counter)
     counter+=1
     if counter == 10000:
-        exit(0)
+        run = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+end = perf_counter()
+print('For loop: 1.489515')
+print(f'While Loop: {end-start}')
+
