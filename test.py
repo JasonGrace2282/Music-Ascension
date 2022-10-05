204f91830f645205da2022a1a555ed1af6ba8781
JasonGrace2282 <aarush.deshpande@gmail.com>
2022-10-05 09:18:35 -0400

    changed game loop to while loop

--- test.py
+++ test.py
@@ -1,26 +1,25 @@
+from itertools import cycle
 from time import perf_counter
 import pygame
 pygame.init()
 screen = pygame.display.set_mode((1200, 790))
 counter = 0
-my_list = [0]
 run = True
 start = perf_counter()
 while run:
     print(counter)
     counter+=1
-    if counter == 10000:
+    if counter == 1000000:
         break
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
 middle = perf_counter()
-for i in my_list:
+for i in cycle((1, 1)):
     print(counter)
     counter+=1
-    my_list.append(i+1)
-    if counter == 20000:
+    if counter == 2000000:
         break
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
